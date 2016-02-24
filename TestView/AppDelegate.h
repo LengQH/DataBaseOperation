61d7fc431e84c78b8eefcbed431922bdcb526ac3
LengQiuHui <1205632644@qq.com>
2016-02-24 21:08:31 +0800

    Initial Commit

---
+++ TestView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestView
+//
+//  Created by ma c on 16/2/24.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
